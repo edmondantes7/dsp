3f609e1a7d945b9f83e59f55db88364cbd5e1cd0
edmondantes7 <richdewey@gmail.com>
2017-09-16 08:47:48 -0700

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,35 @@
-PLACE YOUR CODE HERE
+# Regular Expression Python Code
+# R. Dewey
+
+from collections import OrderedDict
+from collections import Counter
+import sys
+import numpy as np
+import pandas as pd
+import csv
+import re
+
+
+matched_row = []
+matched_row2 = []
+matched_row3 = []
+domain = []
+with open("faculty.csv", "r") as file:
+    # Read file as a CSV delimited by tabs.
+    reader = csv.reader(file, delimiter=',')
+    headers = reader.next()
+    for row in reader:
+        matched_row.append(row[3])
+        matched_row2.append(row[1])
+        matched_row3.append(row[2])
+    #print(matched_row)
+    for emails in matched_row:
+    	domain.append(re.search('@.*', emails).group())
+    	
+
+c = Counter(matched_row2)
+d = Counter(matched_row3)
+f = Counter(domain)
+print(c.items())
+print(d.items())
+print(f.items())
